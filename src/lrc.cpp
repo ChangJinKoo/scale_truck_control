5d412a45765893e69a5d42bcbddb53ad4c74cafc
ChangjinKoo <cjkoo96@gmail.com>
2022-04-28 13:42:02 +0900

    Minor fix

--- src/lrc.cpp
+++ src/lrc.cpp
@@ -260,7 +260,7 @@ void LocalRC::recordData(struct timeval *startTime){
     std::scoped_lock lock(data_mutex_, time_mutex_);
     gettimeofday(&currentTime, NULL);
     time_ = ((currentTime.tv_sec - startTime->tv_sec)) + ((currentTime.tv_usec - startTime->tv_usec)/1000000.0);
-    sprintf(buf, "%.3e,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d", time_, req_time_, tar_dist_, cur_dist_, tar_vel_, cur_vel_, sat_vel_, hat_vel_, alpha_);
+    sprintf(buf, "%.10e,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d", time_, req_time_, tar_dist_, cur_dist_, tar_vel_, cur_vel_, sat_vel_, hat_vel_, alpha_);
     write_file.open(file, std::ios::out | std::ios::app);
     write_file << buf << endl;
   }
