2e0618d0c32d802c102c3ba3a829e9d90c9bda02
ChangjinKoo <cjkoo96@gmail.com>
2022-07-04 21:03:58 +0900

    Add steer angle data in logging file

--- src/lrc.cpp
+++ src/lrc.cpp
@@ -68,6 +68,7 @@ void LocalRC::init(){
   OcrPublisher_ = nodeHandle_.advertise<scale_truck_control::lrc2ocr>(OcrPubTopicName, OcrPubQueueSize);
 
   lrc_mode_ = 0;
+  crc_mode_ = 0;
   lrc_data_ = new ZmqData;
   lrc_data_->src_index = index_;
   lrc_data_->tar_index = 30;  //CRC
@@ -262,14 +263,14 @@ void LocalRC::recordData(struct timeval *startTime){
       }
       read_file.close();
     }
-    write_file << "Time,Request_time,Tar_dist,Cur_dist,Tar_vel,Cur_vel,Est_vel,Sat_vel,Hat_vel,Fi_encoder,Alpha,Fi_camera,Beta,Fi_lidar,Gamma,LRC_mode,CRC_mode" << endl; //seconds
+    write_file << "Time,Request_time,Tar_dist,Cur_dist,Tar_vel,Cur_vel,Est_vel,Sat_vel,Hat_vel,Steer_angle,Fi_encoder,Alpha,Fi_camera,Beta,Fi_lidar,Gamma,LRC_mode,CRC_mode" << endl; //seconds
     flag = true;
   }
   else{
     std::scoped_lock lock(data_mutex_, time_mutex_);
     gettimeofday(&currentTime, NULL);
     time_ = ((currentTime.tv_sec - startTime->tv_sec)) + ((currentTime.tv_usec - startTime->tv_usec)/1000000.0);
-    sprintf(buf, "%.10e,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%d,%d,%d,%d,%d,%d,%d", time_, req_time_, tar_dist_, cur_dist_, tar_vel_, cur_vel_, est_vel_, sat_vel_, hat_vel_, fi_encoder_, alpha_, fi_camera_, beta_, fi_lidar_, gamma_, lrc_mode_, crc_mode_);
+    sprintf(buf, "%.10e,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%d,%d,%d,%d,%d,%d,%d", time_, req_time_, tar_dist_, cur_dist_, tar_vel_, cur_vel_, est_vel_, sat_vel_, hat_vel_, angle_degree_, fi_encoder_, alpha_, fi_camera_, beta_, fi_lidar_, gamma_, lrc_mode_, crc_mode_);
     write_file.open(file, std::ios::out | std::ios::app);
     write_file << buf << endl;
   }
